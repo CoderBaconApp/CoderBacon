f1fa279de655ced8dc76ef7d366a0f265e39a292
Therin Irwin <therin.irwin@gmail.com>
2013-10-19 12:06:23 -0700

    Create message model

---
+++ Bromance/Message.h
@@ -0,0 +1,18 @@
+//
+//  Message.h
+//  Bromance
+//
+//  Created by Therin Irwin on 10/19/13.
+//  Copyright (c) 2013 Pamela Ocampo. All rights reserved.
+//
+
+#import <Parse/Parse.h>
+#import <Foundation/Foundation.h>
+
+@interface Message : NSObject
+
+@property (strong, nonatomic) NSString* text;
+@property (strong, nonatomic) PFUser* sender;
+@property (strong, nonatomic) PFUser* receiver;
+
+@end
---
+++ Bromance/Message.m
@@ -0,0 +1,13 @@
+//
+//  Message.m
+//  Bromance
+//
+//  Created by Therin Irwin on 10/19/13.
+//  Copyright (c) 2013 Pamela Ocampo. All rights reserved.
+//
+
+#import "Message.h"
+
+@implementation Message
+
+@end
