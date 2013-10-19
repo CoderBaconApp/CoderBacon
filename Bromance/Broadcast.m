bce7a57832d2e4b2603bf1f4fa1720145959daa5
Therin Irwin <therin.irwin@gmail.com>
2013-10-19 12:09:58 -0700

    Create broadcast model

---
+++ Bromance/Broadcast.m
@@ -0,0 +1,13 @@
+//
+//  Broadcast.m
+//  Bromance
+//
+//  Created by Therin Irwin on 10/19/13.
+//  Copyright (c) 2013 Pamela Ocampo. All rights reserved.
+//
+
+#import "Broadcast.h"
+
+@implementation Broadcast
+
+@end
