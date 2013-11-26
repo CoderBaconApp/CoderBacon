21c26af50351fd1f219dc4113e6dc54a2cf3cc4f
Pamela Ocampo <pamela.ocampo@gmail.com>
2013-11-25 18:27:44 -0800

    Rewrite App Copyright

--- CoderBacon/MessageReceivedCell.h
+++ CoderBacon/MessageReceivedCell.h
@@ -1,6 +1,6 @@
 //  MessageReceivedCell.h
 //
-//  Copyright (C) 2013 BromanceApp
+//  Copyright (C) 2013 CoderBacon
 //
 //  Licensed under Creative Commons BY-NC-SA
 //  http://creativecommons.org/licenses/by-nc-sa/3.0/deed.en_US
